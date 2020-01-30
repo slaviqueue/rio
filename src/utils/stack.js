5ad84d4614b0fadd6fe77fbc2a95ee6abbcec2ee
slaviqueue <electricsemyon@gmail.com>
2020-01-30 14:48:28 +0200

    written some stack helpers

--- src/utils/stack.js
+++ src/utils/stack.js
@@ -2,6 +2,16 @@ function stack (...elements) {
   return elements
 }
 
+function push (stack, element) {
+  return [element, ...stack]
+}
+
+function head (stack) {
+  return stack[0]
+}
+
 module.exports = {
-  stack
+  stack,
+  push,
+  head
 }
--- test/utils/stack.test.js
+++ test/utils/stack.test.js
@@ -1,11 +1,36 @@
+/* global describe it */
 const expect = require('chai').expect
-const { stack } = require('../../src/utils/stack')
+const { stack, push, head } = require('../../src/utils/stack')
 
-describe('#stack', () => {
-  it ('creates new stack', () => {
-    const callStack = stack()
+describe('Stack', () => {
+  describe('Stack::stack()', () => {
+    it('creates new stack', () => {
+      const callStack = stack()
 
-    expect(callStack).to.haveOwnProperty('length')
-    expect(callStack).to.be.an('array')
+      expect(callStack).to.haveOwnProperty('length')
+      expect(callStack).to.be.an('array')
+    })
+
+    it('adds passed args to stack', () => {
+      const elements = [1, 2, 3]
+
+      expect(stack(...elements)).to.deep.eq(elements)
+    })
+  })
+
+  describe('Stack::push()', () => {
+    it('pushes elements to stack', () => {
+      const callStack = stack(1)
+      const newStack = push(callStack, 2)
+
+      expect(newStack[0]).to.eq(2)
+    })
+  })
+
+  describe('Stack::head()', () => {
+    it('shows head of stack', () => {
+      const callStack = stack(1)
+      expect(head(callStack)).to.eq(1)
+    })
   })
 })
