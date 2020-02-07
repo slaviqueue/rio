973b31024d1421bbbe5815c1cc9bc5c17f399fbb
slaviqueue <electricsemyon@gmail.com>
2020-02-07 17:49:40 +0200

    returned immutability

--- src/subinterpreters/value-declaration.js
+++ src/subinterpreters/value-declaration.js
@@ -3,6 +3,11 @@ const { head } = require('../utils/stack')
 function valueDeclaration (interpret, { callStack }) {
   return (node) => {
     const value = interpret(node.value)
+
+    if (head(callStack)[node.id.value]) {
+      throw new Error(`Constant ${node.id.value} cannot be reassigned`)
+    }
+
     head(callStack)[node.id.value] = value
 
     return value
